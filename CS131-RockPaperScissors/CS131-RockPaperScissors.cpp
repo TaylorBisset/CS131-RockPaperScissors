25269833f3b7f2ec06314edfb66bc1adc10ede54
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-24 14:46:33 -0700

    Initial commit

--- CS131-RockPaperScissors/CS131-RockPaperScissors.cpp
+++ CS131-RockPaperScissors/CS131-RockPaperScissors.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    
+    // End program sequence
+    cout << endl << endl;
+#ifdef _WIN32
+    system("pause");
+#else
+    cout << "Press any key to continue . . . ";
+    cin.get();
+#endif
+    return 0;
 }
