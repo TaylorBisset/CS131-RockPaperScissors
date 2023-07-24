402c8958411d16230854620e3ea52786e1b81b08
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-24 14:58:16 -0700

    Add step 0, copying template code.

--- CS131-RockPaperScissors/CS131-RockPaperScissors.cpp
+++ CS131-RockPaperScissors/CS131-RockPaperScissors.cpp
@@ -18,8 +18,21 @@ Only a portion of tests pass after each step but confirm progress.
 
 int main()
 {
+    /*
+    Step 0. Read starter template and do not change the provided code. 
+    Integer constants are defined for ROCK, PAPER, and SCISSORS. 
+    A Random object is created and a seed is read from input and passed to the Random object. 
+    This supports automated testing and creates predictable results that would otherwise be random.
+    */
     
-    
+    const int ROCK = 0;
+    const int PAPER = 1;
+    const int SCISSORS = 2;
+
+    int seed;
+    cin >> seed;
+    srand(seed);
+
     // End program sequence
     cout << endl << endl;
 #ifdef _WIN32
