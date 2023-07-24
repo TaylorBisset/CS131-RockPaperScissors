78b5218373a967925b020c1546dc301443d418b0
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-24 14:48:28 -0700

    Comment in program specs.

--- CS131-RockPaperScissors/CS131-RockPaperScissors.cpp
+++ CS131-RockPaperScissors/CS131-RockPaperScissors.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+/*
+Program Specifications Write a program to play an automated game of Rock, Paper, Scissors. 
+Two players make one of three hand signals at the same time. 
+Hand signals represent a rock, a piece of paper, or a pair of scissors. 
+Each combination results in a win for one of the players. 
+Rock crushes scissors, paper covers rock, and scissors cut paper. 
+A tie occurs if both players make the same signal. 
+Use a random number generator of 0, 1, or 2 to represent the three signals.
+
+Note: this program is designed for incremental development. 
+Complete each step and submit for grading before starting the next step. 
+Only a portion of tests pass after each step but confirm progress.
+*/
+
 int main()
 {
     
